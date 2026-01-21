aaf8ff5f10095c7514ae9a85990421b8c84b75bd
Bikash-Dushad <bikashghll@gmail.com>
2026-01-21 23:14:25 +0530

    update only when location changes and kafka things

--- sockets/location.socket.js
+++ sockets/location.socket.js
@@ -1,42 +1,58 @@
 const redis = require("../config/redis");
+const { producer } = require("../kafka/producer");
+const Topics = require("../kafka/topics");
 
 module.exports = (io) => {
   io.on("connection", (socket) => {
     //stores the updated captains location to redis
     socket.on("LOCATION_UPDATE", async ({ lat, lng }) => {
-      await redis.geoadd("captains:online", lng, lat, socket.user.id);
-      console.log("location added", lng, lat)
-    });
+      const prevLocation = await redis.geopos(
+        "captains:online",
+        socket.user.id,
+      );
+      if (
+        !prevLocation[0] ||
+        prevLocation[0] === null ||
+        prevLocation[0][0] !== lng ||
+        prevLocation[0][1] !== lat
+      ) {
+        await redis.geoadd("captains:online", lng, lat, socket.user.id);
+        console.log("location added", lng, lat);
 
-    //sends the captains location user
-    socket.on("USER_LOCATION", async ({ lat, lng }) => {
-      try {
-        const captains = await redis.georadius(
-          "captains:online",
-          lng,
-          lat,
-          5,
-          "km",
-          "WITHCOORD",
-        );
-
-        const result = captains.map(([id, [captLng, captLat]]) => ({
-          id,
-          lat: parseFloat(captLat),
-          lng: parseFloat(captLng),
-        }));
-
-        socket.emit("NEARBY_CAPTAINS", result);
-        console.log("nearby captains ", result)
-      } catch (err) {
-        console.error("Error fetching nearby captains:", err);
-        socket.emit("NEARBY_CAPTAINS", []);
+        await producer.send({
+          topic: Topics.CAPTAIN_LOC_UPDATED,
+          messages: [
+            {
+              value: JSON.stringify({
+                id: socket.user.id,
+                lat,
+                lng,
+                timestamp: Date.now(),
+                online: true,
+              }),
+            },
+          ],
+        });
+        console.log("📤 location sent to kafka");
       }
     });
 
     socket.on("disconnect", async () => {
       await redis.zrem("captains:online", socket.user.id);
       console.log("Captain offline");
+      await producer.send({
+        topic: Topics.CAPTAIN_LOC_UPDATED,
+        messages: [
+          {
+            value: JSON.stringify({
+              id: socket.user.id,
+              online: false,
+              timestamp: Date.now(),
+            }),
+          },
+        ],
+      });
+      console.log("📤 offline status sent to kafka");
     });
   });
 };
